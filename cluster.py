e7bfd9507960f191acbb12e6dc88c38f8d799d78
Mary Yohannes <myohanne@broadinstitute.org>
2020-12-11 13:19:24 -0500

    added hail batch script

--- cluster.py
+++ cluster.py
@@ -1,3 +1,5 @@
+## cluster related 
+
 # choose between projects, first run 
 gcloud init 
 
@@ -9,3 +11,28 @@ hailctl dataproc connect mty notebook --zone us-central1-a
 	
 # stop a cluster (which you need to do at the end ALWAYS) 
 hailctl dataproc stop mty --region us-central1
+
+## hail batch related 
+
+## running hail batch on local backend
+
+# import hail batch first 
+import hailtop.batch as hb 
+  
+# define batch
+b = hb.Batch(name='practice1') 
+
+# define job 
+
+# job command to run 
+for degree in ['BA', 'MS']: 
+	for school in ['CU', 'BU']:
+		j = b.new_job(name=f'{degree}-{school}')
+
+# command to run
+j.command(f'echo "I did my {degree} at {school}" ')
+
+
+# run batch
+b.run()
+
