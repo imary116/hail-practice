4cbaf6098e4ab8cc9e413b746dfcc4fda688a26b
Mary Yohannes <myohanne@broadinstitute.org>
2020-12-11 07:38:30 -0500

    adding script

---
+++ cluster.py
@@ -0,0 +1,11 @@
+# choose between projects, first run 
+gcloud init 
+
+# start a cluster on the cloud with hail predownloaded
+hailctl dataproc start mty --region us-central1	
+
+# connect to a jupiter notebook
+hailctl dataproc connect mty notebook --zone us-central1-a 
+	
+# stop a cluster (which you need to do at the end ALWAYS) 
+hailctl dataproc stop mty --region us-central1
