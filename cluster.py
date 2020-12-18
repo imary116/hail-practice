47dc9fc3ea604728ccf9c69450f4ea9887147d57
Mary Yohannes <myohanne@broadinstitute.org>
2020-12-18 11:23:12 -0500

    added service backend ex

--- cluster.py
+++ cluster.py
@@ -1,42 +1,63 @@
-## cluster related 
+#########cluster related######### 
 
-# choose between projects, first run 
+#choose between projects, first run 
 gcloud init 
 
-# start a cluster on the cloud with hail predownloaded
+#start a cluster on the cloud with hail predownloaded
 hailctl dataproc start mty --region us-central1	
 
-# connect to a jupiter notebook
+#connect to a jupiter notebook
 hailctl dataproc connect mty notebook --zone us-central1-a 
 	
-# stop a cluster (which you need to do at the end ALWAYS) 
+#stop a cluster (which you need to do at the end ALWAYS) 
 hailctl dataproc stop mty --region us-central1
 
-## hail batch related 
+#########hail batch related######### 
 
-## running hail batch on local backend
+##running hail batch on LOCAL backend
 
-# import hail batch first 
+#import hail batch first 
 import hailtop.batch as hb 
   
-# define batch
+#define batch
 b = hb.Batch(name='practice1') 
 
-# define job 
-
-# job command to run 
+#define job 
+#job command to run 
 for degree in ['BA', 'MS']: 
 	for school in ['CU', 'BU']:
 		j = b.new_job(name=f'{degree}-{school}')
 		j.command(f'echo "I did my {degree} at {school}" ')
 
 
-# run batch
+#run batch
 b.run()
 
-## Output:
+##Output:
 # I did my BA at CU
 # I did my BA at BU
 # I did my MS at CU
 # I did my MS at BU 
 
+##running hail batch on SERVICE backend 
+
+#import hail batch first
+import hailtop.batch as hb
+
+#setup service backend (don't need to run this command for local backend)
+backend=hb.ServiceBackend(billing_project='myohanne-trial', bucket='MY_BUCKET')
+
+#define batch 
+b=hb.Batch(backend=backend, name='practice2')
+
+#define job
+# job command to run
+for degree in ['BA', 'MS']:
+	for school in ['CU', 'BU']:
+		j = b.new_job(name=f'{degree}-{school}')
+		j.command(f'echo "I did my {degree} at {school}" ')
+
+#run batch 
+b.run()
+
+##Output similar to the local backend example above
